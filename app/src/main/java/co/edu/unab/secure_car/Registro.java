a8e221d622730ad9ff895d904e1c1b39156fb8a1
mateo <mateovillamizar54@gmail.com>
2022-11-06 20:28:02 -0500

    a1

--- app/src/main/java/co/edu/unab/secure_car/Registro.java
+++ app/src/main/java/co/edu/unab/secure_car/Registro.java
@@ -50,72 +50,48 @@ public class Registro extends AppCompatActivity {
         //updateUI(currentUser);
     }
     //Registro de Usuario
-    /*public void registrarUsuario(View view){
+    public void Registro(View view){
+        if (pt_contraseña_registro.getText().toString().equals(pt_confirmar_contraseña.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(pt_correo.getText().toString(), pt_contraseña_registro.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Map<String, Object> map=new HashMap<>();
+                        map.put( "Nombre",pt_usuario_registro.getText());
+                        map.put("Correo",pt_correo.getText());
+                        map.put("Contraseña",pt_contraseña_registro.getText());
 
-        if (pt_contraseña_registro.getText().toString().equals(pt_confirmar_contraseña.getText().toString())){   //Verificar que las contrseñas sean iguales
+                        String id= Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
 
-            mAuth.createUserWithEmailAndPassword(pt_correo.getText().toString(), pt_contraseña_registro.getText().toString())
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()) {
-                                Toast.makeText(getApplicationContext(), "Usuario creado", Toast.LENGTH_SHORT).show();
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                String id = Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
-                                Intent i = new Intent(getApplicationContext(), Login.class);
-                                startActivity(i);
-                                //updateUI(user);
-                            } else{
-                                Toast.makeText(getApplicationContext(), "Error de registro", Toast.LENGTH_SHORT).show();
-                                //updateUI(null);
+                        mdatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task2) {
+
+                                if (task2.isSuccessful()){
+                                    startActivity(new Intent(Registro.this,Login.class));
+                                    finish();
+                                    // StyleableToast.makeText(regitrate.this,"Registro correctamente",R.style.exampleToast).show();
+                                    Toast.makeText(Registro.this, "Registro correctamente", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    //StyleableToast.makeText(regitrate.this,"no se pudo crear datos correctamete",R.style.exampleToast).show();
+                                    Toast.makeText(Registro.this, "no se pudo crear datos ", Toast.LENGTH_SHORT).show();
+                                }
+
                             }
-                        }
-                    });
+                        });
+
+                    }
+                    else {
+                        //  StyleableToast.makeText(regitrate.this,"no se pudo crear datos correctamente",R.style.exampleToast).show();
+                        Toast.makeText(Registro.this, "no se pudo crear datos correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            });
 
         } else {
             Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
         }
-    }*/
-
-    public void Registro(View view){
-        mAuth.createUserWithEmailAndPassword(pt_correo.getText().toString(), pt_contraseña_registro.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Map<String, Object> map=new HashMap<>();
-                    map.put( "Nombre",pt_usuario_registro.getText());
-                    map.put("Correo",pt_correo.getText());
-                    map.put("Contraseña",pt_contraseña_registro.getText());
-
-                    String id= Objects.requireNonNull(mAuth.getCurrentUser()).getUid();
-
-                    mdatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task2) {
-
-                            if (task2.isSuccessful()){
-                                startActivity(new Intent(Registro.this,Login.class));
-                                finish();
-                                // StyleableToast.makeText(regitrate.this,"Registro correctamente",R.style.exampleToast).show();
-                                Toast.makeText(Registro.this, "Registro correctamente", Toast.LENGTH_SHORT).show();
-                            }
-                            else {
-                                //StyleableToast.makeText(regitrate.this,"no se pudo crear datos correctamete",R.style.exampleToast).show();
-                                Toast.makeText(Registro.this, "no se pudo crear datos ", Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    });
-
-                }
-                else {
-                    //  StyleableToast.makeText(regitrate.this,"no se pudo crear datos correctamente",R.style.exampleToast).show();
-                    Toast.makeText(Registro.this, "no se pudo crear datos correctamente", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-        });
-
-    }
-
+        }
 }
